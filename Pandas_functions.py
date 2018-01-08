cb6a22cbfe6e6b83a5aa94e2c1c4670e65a17889
kristjankostelec <kristjan.kostelec@gmail.com>
2018-01-08 22:59:08 +0100

    Beginning of working with pandas

---
+++ Pandas_functions.py
@@ -0,0 +1,12 @@
+#this is a python file, meant for data analysis of tennis players
+
+def function1 ():
+    print('Hello, pandas!')
+    
+def function2 (ime):
+    return ('Congratulations' + ime + ', you successfully installed jupyter notebook!')
+
+with open ('podatki_za_obdelavo.txt', encoding='utf-8') as pod:
+    podatki = pod.read()
+
+a=10
